bb574f4209e87996004745976327310ad253fdcf
Nour <nourpow@gmail.com>
2024-08-14 21:39:05 +0100

    start adding context

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,5 +1,31 @@
-import { Clapperboard, Home, Library, Repeat } from "lucide-react";
-import { Children, ElementType } from "react";
+import {
+  ChevronDown,
+  Clapperboard,
+  Home,
+  Library,
+  Repeat,
+  ShowerHead,
+  ChevronUp,
+  UserPen,
+  History,
+  ListVideo,
+  Youtube,
+  SquarePlay,
+  Clock8,
+  ThumbsUp,
+  Scissors,
+  TrendingUp,
+  ShoppingBag,
+  Music,
+  RadioTower,
+  Gamepad2,
+  Newspaper,
+  Trophy,
+  GraduationCap,
+  Shirt,
+} from "lucide-react";
+import { Children, ElementType, useState } from "react";
+import { playlists, subscriptions } from "../data/sidebar";
 
 function Sidebar() {
   return (
@@ -17,20 +43,102 @@ function Sidebar() {
       </aside>
 
       {/* sidebar for large screens */}
-      <aside className="w-56 lg:sticky top-0 absolute overflow-y-auto scrollbar-hidden pb-4 flex flex-col gap-2 px-2">
+      <aside className="w-56 lg:sticky top-0 absolute overflow-y-auto scrollbar-hidden pb-4 lg:flex flex-col gap-2 px-2 hidden ">
         {/*section1: home + shorts + subscriptions */}
         <LargeSidebarSection>
           {/*home */}
 
-          <LargeSidebarItem isActive={false} Icon={Home} title="Home" url="/" />
+          <LargeSidebarItem isActive IconOrImgUrl={Home} title="Home" url="/" />
 
           {/*Shorts */}
-          <LargeSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
+          <LargeSidebarItem
+            IconOrImgUrl={Repeat}
+            title="Shorts"
+            url="/shorts"
+          />
           {/* subscriptions */}
           <LargeSidebarItem
-            Icon={Clapperboard}
+            IconOrImgUrl={Clapperboard}
             title="Subscriptions"
             url="/subscriptions"
+          />
+        </LargeSidebarSection>
+        <hr />
+        <LargeSidebarSection title="You" visibleItemCount={7}>
+          <LargeSidebarItem
+            IconOrImgUrl={UserPen}
+            title="Your channel"
+            url="/"
+          />
+          <LargeSidebarItem IconOrImgUrl={History} title="History" url="/" />
+
+          <LargeSidebarItem
+            IconOrImgUrl={SquarePlay}
+            title="Your videos"
+            url="/"
+          />
+          <LargeSidebarItem IconOrImgUrl={Clock8} title="Watch later" url="/" />
+          <LargeSidebarItem
+            IconOrImgUrl={ThumbsUp}
+            title="Liked videos"
+            url="/"
+          />
+          <LargeSidebarItem
+            IconOrImgUrl={Scissors}
+            title="Your clips"
+            url="/"
+          />
+          {playlists.map((playlist) => (
+            <LargeSidebarItem
+              key={playlist.id}
+              IconOrImgUrl={ListVideo}
+              title={playlist.name}
+              url={`/playlist?list=${playlist.id}`}
+            />
+          ))}
+        </LargeSidebarSection>
+        <hr />
+        <LargeSidebarSection title="Subscriptions" visibleItemCount={3}>
+          {subscriptions.map((subscription) => (
+            <LargeSidebarItem
+              key={subscription.id}
+              IconOrImgUrl={subscription.imgUrl}
+              title={subscription.channelName}
+              url={`/@${subscription.id}`}
+            />
+          ))}
+        </LargeSidebarSection>
+        <hr />
+        <LargeSidebarSection title="Explore" visibleItemCount={4}>
+          <LargeSidebarItem
+            IconOrImgUrl={TrendingUp}
+            title="Trending"
+            url="/"
+          />
+          <LargeSidebarItem
+            IconOrImgUrl={ShoppingBag}
+            title="Shopping"
+            url="/"
+          />
+          <LargeSidebarItem IconOrImgUrl={Music} title="Music" url="/" />
+          <LargeSidebarItem
+            IconOrImgUrl={Clapperboard}
+            title="Movies & TV"
+            url="/"
+          />
+          <LargeSidebarItem IconOrImgUrl={RadioTower} title="Live" url="/" />
+          <LargeSidebarItem IconOrImgUrl={Gamepad2} title="Gaming" url="/" />
+          <LargeSidebarItem IconOrImgUrl={Newspaper} title="News" url="/" />
+          <LargeSidebarItem IconOrImgUrl={Trophy} title="Sports" url="/" />
+          <LargeSidebarItem
+            IconOrImgUrl={GraduationCap}
+            title="Learning"
+            url="/"
+          />
+          <LargeSidebarItem
+            IconOrImgUrl={Shirt}
+            title="Fashion & Beauty"
+            url="/"
           />
         </LargeSidebarSection>
       </aside>
@@ -59,14 +167,14 @@ function SmallSidebarItem({ Icon, title, url }: SmallSidebarItemProps) {
 }
 
 type LargeSidebarItemProps = {
-  Icon: ElementType;
+  IconOrImgUrl: ElementType | string;
   title: string;
   url: string;
   isActive?: boolean;
 };
 
 function LargeSidebarItem({
-  Icon,
+  IconOrImgUrl,
   title,
   url,
   isActive = false,
@@ -78,7 +186,16 @@ function LargeSidebarItem({
         isActive ? "bg-gray-200 font-bold" : undefined
       }`}
     >
-      <Icon className="w-6 h-6" />
+      {typeof IconOrImgUrl === "string" ? (
+        <img
+          src={IconOrImgUrl}
+          alt="Channel Icon"
+          className="w-6 h-6 rounded-full"
+        />
+      ) : (
+        <IconOrImgUrl className="w-6 h-6" />
+      )}
+
       <div className="text-sm whitespace-nowrap overflow-hidden text-ellipsis">
         {title}
       </div>
@@ -98,7 +215,16 @@ function LargeSidebarSection({
   visibleItemCount = Number.POSITIVE_INFINITY,
 }: LargeSidebarSectionProps) {
   const childrenArray = Children.toArray(children).flat();
-  const visibleChildren = childrenArray.slice(0, visibleItemCount);
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const showExpandButton = childrenArray.length > visibleItemCount;
+
+  const visibleChildren = isExpanded
+    ? childrenArray
+    : childrenArray.slice(0, visibleItemCount);
+
+  const ButtonIcon = isExpanded ? ChevronUp : ChevronDown;
 
   return (
     <div>
@@ -111,6 +237,15 @@ function LargeSidebarSection({
         {title}
       </div>
       {visibleChildren}
+      {showExpandButton && (
+        <button
+          onClick={() => setIsExpanded((e) => !e)}
+          className={`hover:bg-gray-200 px-6 py-4 w-full rounded-lg flex items-center gap-5 `}
+        >
+          <ButtonIcon className="h-6 w-6" />
+          <div className="">{isExpanded ? "Show less" : "Show more"}</div>
+        </button>
+      )}
     </div>
   );
 }
